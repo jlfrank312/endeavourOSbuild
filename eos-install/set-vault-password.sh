5dd9eabf6c0020b24062ad978726530aa5689af3
jlfrank312 <jlfrank312@gmail.com>
2026-04-18 22:25:46 -0700

    updated to export to ~/.config/ansible

--- eos-install/set-vault-password.sh
+++ eos-install/set-vault-password.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-printf '%s' "$1" > ~/setup/vault-password
-chmod 600 ~/setup/vault-password
+mkdir -p ~/.config/ansible
+printf '%s' "$1" > ~/.config/ansible/vault-password
+chmod 600 ~/.config/ansible/vault-password
 echo "Vault password set."
